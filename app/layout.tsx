274797ecbcf6cc0e53bea5c06b4b2e53c2233149
victorronchin <victor@hydraoss.io>
2026-05-07 11:00:39 +0200

    test break pixel 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
+          'https://connect.faceboook.net/en_US/fbevents.js');
           fbq('init', '${PIXEL_ID}');
           fbq('track', 'PageView');
         `}</Script>
